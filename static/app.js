925116a89fbac224f76f55cb347b0ff2f5ff8ab6
ZakEspley <zespley@gmail.com>
2018-02-12 22:38:53 -0800

    Added graph functionality and Run simulation

--- static/app.js
+++ static/app.js
@@ -27,6 +27,7 @@ $(document).ready(function(){
     app.renderer.autoResize = true;
     app.renderer.resize(stageWidth, stageHeight);
     var play = false;
+    var running = false;
 
     window.addEventListener("resize", function (e) {
         console.log($(".container").width());
@@ -37,15 +38,20 @@ $(document).ready(function(){
         //Place Origin in Bottom Right
         app.stage.position.y = app.renderer.height;
         app.stage.scale.y = -1;
-        mb.x = 0.33*app.stage.width;
-        mt.x = 0.66*stageWidth;
+        mb.setX(0.33*app.renderer.width);
+        mt.setX(0.66*app.renderer.width);
         mt.width = 0.25*stageWidth;
         mb.width = 0.25*stageWidth;
     })
 
     function updateSimulation(event, ui) {
-        lambda = lambdaSlide.slider('value');
-        kappa = kappaSlide.slider('value');
+        if (ui.handle.parentElement == lambdaSlide[0]) {
+            lambda = ui.value;
+            lamVal.val(lambda);
+        } else if (ui.handle.parentElement == kappaSlide[0]) {
+            kappa = ui.value;
+            kapVal.val(kappa);
+        }
         mt.sand.height = lambda*mt.border.height;
         mb.sand.height = (1-lambda)*mb.border.height;
         mt.sand.alpha = (1/kMax**j)*kappa**j
@@ -54,8 +60,36 @@ $(document).ready(function(){
         mb.mg_vec.height = mw*g*kappa*(1-lambda);
         at = (1/(lambda*kappa) - 1)*g;
         ab = (1/((1-lambda)*kappa)-1)*g;
-        lamVal.val(lambda);
-        kapVal.val(kappa);
+        functionPlot({
+            target: '#canchart',
+            data: [
+                {
+                    fn: '(1/({0}*x)-1)'.format(kappa.toString())
+                }, {
+                    fn: '(1/((1-x)*{0})-1)'.format(kappa.toString())
+                }, {
+                    fn: '1/({0}*x) + 1/((1-x){0}) -2'.format(kappa.toString())
+                }, {
+                    x: 'x={0}'.format(lambda),
+                    y: 'y=t',
+                    fnType: 'parametric',
+                    graphType: "polyline",
+                    range: [-1,10],
+                    nSamples:4
+                }
+            ],
+            xAxis:{
+                label:"Lambda",
+                domain: [0,1]
+            },
+            yAxis:{
+                domain:[-1,3]
+            },
+            annotations: [{
+                x: lambda,
+                text: "Current Lambda",
+            }]
+        });
 
     }
 
@@ -71,7 +105,9 @@ $(document).ready(function(){
         step: 0.01,
         value: 0.5,
         slide: updateSimulation,
-        change: updateSimulation
+        change: updateSimulation,
+        stop: updateSimulation,
+        start: updateSimulation
     }
     )
 
@@ -83,7 +119,9 @@ $(document).ready(function(){
         step: 0.1,
         value: 2,
         slide: updateSimulation,
-        change: updateSimulation
+        change: updateSimulation,
+        stop: updateSimulation,
+        start: updateSimulation
     })
 
 
@@ -99,7 +137,7 @@ $(document).ready(function(){
     kapVal.val(kappa);
 
 
-    var mt = new Can("mt", 0.66 * stageWidth, 0.5 * stageHeight, 0.25 * stageWidth, 0.25 * stageHeight,
+    var mt = new Can("mt", 0.66 * app.renderer.width, 0.5 * stageHeight, 0.25 * stageWidth, 0.25 * stageHeight,
         0xe7990a, mtp, mw, g, kappa);
     var mb = new Can("mb", 0.33 * app.renderer.width, 0.5 * stageHeight, 0.25 * stageWidth, 0.25 * stageHeight,
         0xe7990a, mbp, mw, g, kappa);
@@ -108,13 +146,13 @@ $(document).ready(function(){
     var j = 0.3;
     // mt.sand.alpha = (1/Math.PI * Math.asin(2/kMax*kappa-1)+1/2);
     // mb.sand.alpha = (1/Math.PI * Math.asin(2/kMax*kappa-1)+1/2);
-    mt.sand.aplha = (1/kMax**j)*kappa**j;
-    mb.sand.aplha = (1/kMax**j)*kappa**j;
+    mt.sand.alpha = (1/kMax**j)*kappa**j;
+    mb.sand.alpha = (1/kMax**j)*kappa**j;
 
     var playTexture = PIXI.Texture.fromImage('/static/play.svg');
     var pauseTexture = PIXI.Texture.fromImage('/static/pause.svg')
 
-    function runSim() {
+    function playSim() {
         if (play){
             controlButton.setTexture(playTexture);
         } else {
@@ -123,10 +161,21 @@ $(document).ready(function(){
         play = !play;
     }
 
+    function runSim() {
+        reset();
+        lambdaSlide.slider('value', 0.99);
+        running = true;
+        playSim();
+    }
+
     function reset() {
         time = 0;
         mb.y = mb.y0;
         mt.y = mt.y0;
+        mt.vy = 0;
+        mb.vy = 0;
+        counter = 0;
+        running=false;
     }
 
     var controlButton = new PIXI.Sprite(playTexture);
@@ -135,37 +184,158 @@ $(document).ready(function(){
     controlButton.buttonMode = true;
     controlButton.x=20;
     controlButton.y=20;
-    controlButton.on('pointerdown', runSim);
+    controlButton.on('pointerdown', playSim);
 
     resetButton = new PIXI.Sprite.fromImage("/static/refresh-ccw.svg");
     resetButton.interactive = true;
     resetButton.buttonMode = true;
-    resetButton.x = 60;
+    resetButton.x = 100;
     resetButton.y = 20;
     resetButton.on("pointerdown", reset);
+
+    runButton = new PIXI.Sprite.fromImage("/static/run.png");
+    runButton.interactive = true;
+    runButton.buttonMode = true;
+    runButton.scale.y=-0.7;
+    runButton.scale.x=0.7
+    runButton.x = 50;
+    runButton.y = 50;
+    runButton.on('pointerdown', runSim);
+    
 
     app.stage.addChild(mt);
     app.stage.addChild(mb);
     app.stage.addChild(controlButton);
     app.stage.addChild(resetButton);
+    app.stage.addChild(runButton);
     document.getElementById('display').appendChild(app.view);
-    var timescale = 0.05;
+    var timescale = 0.00025;
     var time = 0;
+    var counter = 0
     app.ticker.add(delta => gameloop(delta));
-
     function gameloop(delta) {
         
         if (play) {
             time += delta*timescale;
-            mt.y = mt.y0 + 1/2*at*(time)**2;
-            mb.y = mb.y0 + 1/2*ab*time**2;
+            counter += 1;
+            mt.y = mt.y + mt.vy*time;
+            mb.y = mb.y + mb.vy*time;
+            mt.vy = mt.vy + at*time;
+            mb.vy = mb.vy + ab*time;
+            // mt.y = mt.y0 + 1/2*at*(time)**2;
+            // mb.y = mb.y0 + 1/2*ab*time**2;
+            if (running && counter%3==0 && lambda>=0.02) {
+                lambdaSlide.slider('value', lambda-0.01);
+            }
         }
     }
+
+    //
+    //  Charts are below here
+    //
+    // var lambdaX = range(0, 1.01, 0.01);
+    // var aty = lambdaX.map(l => (1/(l*kappa) -1)*g);
+    // var aty = lambdaX.multiply(2);
+    // var atb = (1/((1-lambdaX)*kappa) -1)*g;
+    // var data1 = concateV(lambdaX, aty);
+    // var data1 = [[1,1], [3,2]]
+
     
+    // $('#canchart').highcharts({
+    //     chart: {
+    //         type: 'line'
+    //     },
+    //     plotOptions: {
+    //         marker: {
+    //             enabled: false
+    //         },
+    //         linewidth: 2,
+    //     },
+    //     series : [
+    //         {type:"line",
+    //         data:data1}
+    //     ]
+    // })
+
+    // var trace1 = {
+    //     type: 'scatter',
+    //     x: lambdaX,
+    //     y: aty,
+    //     name: "Acceleration of Top Can",
+    //     line: {
+    //         color: '#e8e20b',
+    //         width: 3
+    //     }
+    // };
+    
+    // var data1 = [trace1];
+
+    // Plotly.newPlot('canchart', data1);
+    functionPlot({
+        target: '#canchart',
+        data: [
+            {
+                fn: '(1/({0}*x)-1)'.format(kappa.toString()),
+                text: "a_t"
+            }, {
+                fn: '(1/((1-x)*{0})-1)'.format(kappa.toString())
+            }, {
+                fn: '1/({0}*x) + 1/((1-x){0}) -2'.format(kappa.toString())
+            }, {
+                x: 'x={0}'.format(lambda),
+                y: 'y=t',
+                fnType: 'parametric',
+                graphType: "polyline",
+                range: [-1,10],
+                nSamples:4
+            }
+        ],
+        xAxis:{
+            label:"Lambda",
+            domain: [0,1]
+        },
+        yAxis:{
+            domain:[-1,3]
+        },
+        annotations: [{
+            x: lambda,
+            text: "Current Lambda",
+        }]
+    });
 
     
 });
 
+function range(start, stop, step=1) {
+    var range = [];
+    i=0;
+    while (start < stop) {
+        range[i] = start;
+        start += step;
+        i += 1;
+    }
+    return range
+}
+
+function concateV(a1, a2) {
+    i = 0;
+    a = [];
+    while (i < a1.length) {
+        a[i] = [a1[i], a2[i]];
+        i += 1;
+    }
+
+    return a;
+
+}
+
+String.prototype.format = function() {
+    a = this;
+    for (k in arguments) {
+      a = a.replace(new RegExp("\\{" + k + "\\}", 'g'), arguments[k])
+    }
+    return a
+  }
 
 class Arrow extends PIXI.Container {
     constructor(x0,y0,dx,dy, color, lw=1) {
@@ -200,6 +370,8 @@ class Can extends PIXI.Container {
         this.y0 = this.y;
         this.xc = x;
         this.yc = y;
+        this.vx = 0;
+        this.vy = 0;
         this.height = height;
         this.width = width;
         
@@ -228,7 +400,14 @@ class Can extends PIXI.Container {
         this.addChild(this.fb_vec);
         this.addChild(this.mg_vec);
         this.addChild(this.name);
+    }
 
+    setX(x) {
+        this.x = x-this.width/2;
+    }
+
+    setY(y) {
+        this.y = y-this.height/2;
     }
 }
 
